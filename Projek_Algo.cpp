c070578c25fce56e9088375c3b7995ea93149c8c
tikodjati08@gmail.com <tikodjati08@gmail.com>
2025-05-24 19:59:31 +0700

    Buat struct linked list node schedule dan assignment

--- Projek_Algo.cpp
+++ Projek_Algo.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 using namespace std;
 
+struct node_schedule
+{
+    char nama_mks[50], hari_mks[50];
+    int jmlh_sks_mks, tingkat_kesulitan_mks;
+    node_schedule *next; // pointer ke node berikutnya
+};
+
+// node linked list assignment
+struct node_assignment
+{
+    char nama_mka[50], hari_mka[50], status[20];
+    int jmlh_sks_mka, tingkat_kesulitan_mka;
+    node_assignment *next; // pointer ke node berikutnya
+};
+
 int main(){
-    cout << "TESSSS";
-    cout << "TESSSS 2";
-    cout << "tess3";
+    
 }
