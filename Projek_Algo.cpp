e706a8048bf7ccbb09d88c03dfa6db3d22a4e0a9
oktfiaa <okta77131@gmail.com>
2025-05-24 20:02:45 +0700

    Deklarasi Kepala,Ekor,Bantu linked list dan buat menu utama

--- Projek_Algo.cpp
+++ Projek_Algo.cpp
@@ -16,6 +16,32 @@ struct node_assignment
     node_assignment *next; // pointer ke node berikutnya
 };
 
+// buat kepala/head, ekor, bantu linked lisnya
+node_schedule *kepala_s = NULL;
+node_schedule *ekor_s = NULL;
+node_schedule *bantu_s = NULL;
+node_assignment *kepala_a = NULL;
+node_assignment *ekor_a = NULL;
+node_assignment *bantu_a = NULL;
+
+void menu(int &pilmenu)
+{
+
+    cout << "============= MENU =============" << endl;
+    cout << "================================" << endl;
+    cout << "1. Schedule & Assignment Input" << endl;
+    cout << "2. Schedule & Assignment Show" << endl;
+    cout << "3. Leasson Plan" << endl;
+    cout << "4. Check-List Lesson" << endl;
+    cout << "5. Notes" << endl;
+    cout << "0. Exit Program" << endl;
+    cout << "================================" << endl;
+    cout << " Select the Menu : ";
+    cin >> pilmenu;
+    cout << "================================" << endl;
+    system("clS");
+}
+
 int main(){
     
 }
